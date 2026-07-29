38f8d086d939b438b1815800b93b2ab0514fa49a
izen57 <golovoslon@gmail.com>
2026-07-29 08:00:11 +0200

    Закомментировал базовую аутентификацию

--- Web_site/settings.py
+++ Web_site/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
